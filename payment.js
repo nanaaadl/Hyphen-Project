f82c98ce683c89ddd02c0358f63ac8b8a2f52121
nanaaadl <122984634+nanaaadl@users.noreply.github.com>
2023-05-30 15:54:47 +0530

    Create payment.js

---
+++ payment.js
@@ -0,0 +1,104 @@
+import React from 'react'
+
+import styles from './mac-book-pro161.module.css'
+
+const MacBookPro161 = (props) => {
+  return (
+    <div className={styles['container']}>
+      <div className={styles['mac-book-pro161']}>
+        <div className={styles['frame1']}></div>
+        <img
+          src="/image115-jv6tbk-700h.png"
+          alt="image115"
+          className={styles['image1']}
+        />
+        <img
+          src="/ellipse118-c8lv-500w.png"
+          alt="Ellipse118"
+          className={styles['ellipse1']}
+        />
+        <img
+          src="/line1110-lt8i.svg"
+          alt="Line1110"
+          className={styles['line1']}
+        />
+        <img src="/line222-gog.svg" alt="Line222" className={styles['line2']} />
+        <img
+          src="/rectangle329-bndp-800w.png"
+          alt="Rectangle329"
+          className={styles['rectangle3']}
+        />
+        <img
+          src="/image328-5fvd-200h.png"
+          alt="image328"
+          className={styles['image3']}
+        />
+        <span className={styles['text']}>
+          <span>Name of the card holder</span>
+        </span>
+        <img
+          src="/line3211-h7fe.svg"
+          alt="Line3211"
+          className={styles['line3']}
+        />
+        <span className={styles['text02']}>
+          <span>Card Number</span>
+        </span>
+        <img
+          src="/line4213-fpj.svg"
+          alt="Line4213"
+          className={styles['line4']}
+        />
+        <span className={styles['text04']}>
+          <span>Expiry date</span>
+        </span>
+        <img
+          src="/line5216-l1v8.svg"
+          alt="Line5216"
+          className={styles['line5']}
+        />
+        <img
+          src="/line6217-44z.svg"
+          alt="Line6217"
+          className={styles['line6']}
+        />
+        <img
+          src="/line7218-w5e.svg"
+          alt="Line7218"
+          className={styles['line7']}
+        />
+        <span className={styles['text06']}>
+          <span>CVV</span>
+        </span>
+        <img
+          src="/line8220-a9lf.svg"
+          alt="Line8220"
+          className={styles['line8']}
+        />
+        <img
+          src="/rectangle4221-vt7k-200h.png"
+          alt="Rectangle4221"
+          className={styles['rectangle4']}
+        />
+        <span className={styles['text08']}>
+          <span>PAY NOW</span>
+        </span>
+        <img
+          src="/image4224-nr7-200h.png"
+          alt="image4224"
+          className={styles['image4']}
+        />
+        <img
+          src="/image5226-z4x-200h.png"
+          alt="image5226"
+          className={styles['image5']}
+        />
+        <span className={styles['text10']}>
+          <span>Payment</span>
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default MacBookPro161
